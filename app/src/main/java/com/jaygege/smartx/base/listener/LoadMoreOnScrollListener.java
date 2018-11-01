ce1f4c9ce2378493156516d70b9cd37fdc1873c8
geyan <2277087813@qq.com>
2018-11-01 12:29:09 +0800

    [MDF]修改上拉加载更多监听

--- app/src/main/java/com/jaygege/smartx/base/listener/LoadMoreOnScrollListener.java
+++ app/src/main/java/com/jaygege/smartx/base/listener/LoadMoreOnScrollListener.java
@@ -14,6 +14,7 @@ public abstract class LoadMoreOnScrollListener extends RecyclerView.OnScrollList
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
+        // dy大于0，表示正在向上滑动；小于等于0，表示停止或者向下滑
         isScrollUp = dy > 0;
     }
 
@@ -21,8 +22,9 @@ public abstract class LoadMoreOnScrollListener extends RecyclerView.OnScrollList
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
         super.onScrollStateChanged(recyclerView, newState);
         LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        // 在不滑动的前提下
         if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-            // 不滑动时
+            // 获取最后一个完全显示的itemPosition
             int lastItemPos = layoutManager.findLastCompletelyVisibleItemPosition();
             int itemCount = layoutManager.getItemCount();
             // 判断是否已经滑动到最后一个item，且此时是向上滑动
